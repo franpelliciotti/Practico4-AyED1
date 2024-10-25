8b2d119ab3bce85fe5665e1378fc573d028efed9
franpeliciotti <franpelliciotti00@gmail.com>
2024-10-25 09:32:52 -0300

    Implementacion erronea

--- Plantilla - Implementación de Listas/Plantilla - Implementación de Listas/ListaArray.java
+++ Plantilla - Implementación de Listas/Plantilla - Implementación de Listas/ListaArray.java
@@ -6,38 +6,97 @@ public class ListaArray<T> implements Lista<T>
 {
     private T[] elems;
     private int maxSize;
+    private int indice;
 
     @SuppressWarnings("unchecked")
     public ListaArray(int maxSize){
         this.elems = (T[]) new Object[maxSize];
         this.maxSize = maxSize;
+        this.indice = 0; //Cantidad de elementos
     }
 
     public void insertarFin(T item){
-        //Completar
+        if(indice > maxSize) throw new IllegalStateException("Lista llena");
+        elems[indice] = item;
     }
 
     public void insertarInicio(T elem){
-        //Completar
+        if(esVacio(elems)){
+            indice = 1;
+            elems[0] = elem;
+        } else {
+            moverAdelante();
+        }
     }
 
     public void insertarPos(T elem, int pos){
-        //Completar
+        if(pos > maxSize) throw new IllegalArgumentException("La posición no existe.");
+        if(pos == maxSize && elems[pos] != null) throw new IllegalStateException("La última posición está llena.");
+        if(esVacio(elems)){
+            indice = pos;
+            elems[pos] = elem;
+        } else {
+            elems[pos] = elem;
+        }
     }
 
     public boolean eliminar(T item){
-        //Completar
+        int posicion = buscar(item);
+        boolean eliminado = true;
+        if(posicion == (-1)){
+            eliminado = false;
+        } else {
+            moverAtras(posicion + 1);
+        }     
+        return eliminado;
     }
 
     public int buscar(T item){
-        //Completar
+        int posicionEncontrado = (-1);
+        for(int i = 0; i < maxSize; i++){
+            if(elems[i] == null){ //Para evitar problemas con comparación null
+                posicionEncontrado = (-1);           
+            } else if(elems[i].equals(item)){ 
+                posicionEncontrado = (i + 1);
+            }
+        }
+        return posicionEncontrado;
     }
 
     public int longitud(){
-        //Completar
+        return indice;
     }
 
     public void imprimir(){
-        //Completar
+        if(esVacio(elems)){
+            System.out.println("La lista está vacía.");
+        } else {
+            for(int i = 0; i < maxSize; i++){
+                System.out.println("Elemento en posición " + (i + 1) + ": " + elems[i]);
+            }
+        }
+    }
+    
+    private boolean esVacio(T[] arreglo){
+        boolean esVacio = true;
+        for(int i = 0; i < maxSize; i++){
+            if(arreglo[i] != null){
+                esVacio = false;
+            }
+        }
+        return esVacio;
+    }
+    
+    private void moverAdelante(){
+        if(indice > maxSize) throw new IllegalStateException("Lista llena");
+        for(int i = maxSize - 1; i > 0 && indice <= maxSize; i--) {
+            elems[i] = elems[i - 1];
+        }
+    }
+    
+    private void moverAtras(int tope){
+        for(int i = maxSize - 2; i >= tope; i--) {
+            elems[i + 1] = elems[i];
+        }
     }
 }
