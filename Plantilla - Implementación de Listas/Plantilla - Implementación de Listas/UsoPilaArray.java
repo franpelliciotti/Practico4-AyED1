42ccc5fdff1521655867fbdb82ae14a3d4443dbc
franpeliciotti <franpelliciotti00@gmail.com>
2024-11-02 13:41:16 -0300

    .

---
+++ Plantilla - Implementación de Listas/Plantilla - Implementación de Listas/UsoPilaArray.java
@@ -0,0 +1,10 @@
+
+public class UsoPilaArray {
+    private static void cargarPila(PilaListaArray<Integer> pila){
+        pila.apilar(5);
+        pila.apilar(4);
+        pila.desapilar();
+        pila.apilar(3);
+        pila.apilar(2);
+    }
+}
